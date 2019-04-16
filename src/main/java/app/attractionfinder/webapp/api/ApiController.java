68f40a12693bd9193040d854c19fbe279988d8f0
David C. Bradley <david@dbradley771.com>
2019-04-16 02:15:58 -0500

    Allow Cross Origin for API

--- src/main/java/app/attractionfinder/webapp/api/ApiController.java
+++ src/main/java/app/attractionfinder/webapp/api/ApiController.java
@@ -1,11 +1,13 @@
 package app.attractionfinder.webapp.api;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping(path = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ApiController {
 	@GetMapping
